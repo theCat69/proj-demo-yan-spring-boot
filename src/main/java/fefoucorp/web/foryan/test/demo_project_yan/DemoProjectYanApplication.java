aa634e524eac8d8f3a37e8be9d54ba798cf9663f
Felix Vadcard <vadcard.felix@gmail.com>
2024-12-31 14:11:43 +0100

    first commit working example with liquibase, postgresql and spring boot web

---
+++ src/main/java/fefoucorp/web/foryan/test/demo_project_yan/DemoProjectYanApplication.java
@@ -0,0 +1,13 @@
+package fefoucorp.web.foryan.test.demo_project_yan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectYanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectYanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fefoucorp/web/foryan/test/demo_project_yan/DemoProjectYanApplicationTests.java
@@ -0,0 +1,13 @@
+package fefoucorp.web.foryan.test.demo_project_yan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectYanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
